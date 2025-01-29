e2fcfb557c7ef39089a884e8d00d6c555f3ec2aa
Rahul Arabic <arabicrahul04@gmail.com>
2025-01-29 14:13:59 +0530

    Backend - Added Refresh Access Token API

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   loginController,
   logoutController,
+  refreshAccessTokenController,
   registerController,
 } from "../controllers/user.controllers.js";
 import uploadMiddleware from "../middlewares/multer.middlewares.js";
@@ -17,5 +18,7 @@ const router = Router();
 router.route("/register").post(registerController);
 router.route("/login").post(loginController);
 router.route("/logout").post(verifyJWT, logoutController);
+
+router.route("/refresh-access-token").post(refreshAccessTokenController);
 
 export default router;
