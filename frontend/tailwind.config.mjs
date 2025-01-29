ec24089cdb9244dcab6890f7921aa4703269e7ba
Rahul Arabic <arabicrahul04@gmail.com>
2025-01-29 16:29:12 +0530

    Frontend - Initialized Next JS

---
+++ frontend/tailwind.config.mjs
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+      center: true,
+      padding: "1rem",
+    },
+    extend: {
+      colors: {
+        light: "#FEFEFE",
+        lightAlt: "#C0C1C5",
+        dark: "#0C0C0C",
+        darkAlt: "#161A22",
+        primary: "#D1D812",
+      },
+    },
+  },
+  plugins: [],
+};
