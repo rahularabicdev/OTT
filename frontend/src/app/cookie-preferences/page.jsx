4706b71a4039beded35ad8ebd90c901ead5e2882
Rahul Yadav <arabicrahul04@gmail.com>
2025-02-04 14:12:51 +0530

    Frontend - Added Cookie Preferences Page

---
+++ frontend/src/app/cookie-preferences/page.jsx
@@ -0,0 +1,119 @@
+const CookiePreferences = () => {
+  return (
+    <>
+      <section className="section"></section>
+      <section className="section">
+        <div className="container">
+          <h4 className="heading mb-4">General Description</h4>
+          <p className="mb-3">
+            This cookie tool will help you understand the use of cookies on the
+            Spectraflic service, and how you can control the use of these
+            cookies.
+          </p>
+          <p className="mb-3">
+            Privacy settings in most browsers allow you to prevent your browser
+            from accepting some or all cookies, notify you when it receives a
+            new cookie, or disable cookies altogether. If your browser disables
+            all cookies, then information will not be collected or stored via
+            the cookies listed in this tool. This means that your use of the
+            Spectraflic service may be impaired.
+          </p>
+          <p className="mb-3">
+            Please note that when you use this cookie tool to opt out of certain
+            cookies, your opt out preferences are recorded by placing a cookie
+            on your device. Therefore, your browser must be configured to accept
+            cookies in order for your preferences to take effect. Also, if you
+            delete or clear your cookies, or change your web browser, you will
+            need to reset your cookie preferences.
+          </p>
+          <p className="mb-3">
+            For more information on our use of cookies, please visit the Cookies
+            and Internet Advertising section of our Privacy Statement.
+          </p>
+
+          <hr className="bg-darkAlt h-[1px] my-7" />
+
+          <h4 className="heading mb-4">Essential Cookies</h4>
+          <p className="mb-3">
+            These cookies are strictly necessary to provide the Spectraflic
+            service. For example, we and our Service Providers may use these
+            cookies to authenticate and identify users when they use our
+            websites so we can provide our service to them. They also help us to
+            administer and operate our business; for safety, security and fraud
+            prevention; and to comply with law and enforce our Terms of Use. As
+            these cookies are strictly necessary to provide our service, you
+            cannot opt out of them.
+          </p>
+          <p className="mb-3">
+            Lifespan: Most cookies are session cookies (e.g. only active until
+            you close your browser). Some cookies are active for a longer time,
+            ranging from 3 to 12 months. The cookies used to prevent fraud and
+            maintain the security or our services are active for a maximum
+            period of 24 months.
+          </p>
+
+          <hr className="bg-darkAlt h-[1px] my-7" />
+
+          <h4 className="heading mb-4">
+            First Party Performance and Functionality Cookies
+          </h4>
+          <p className="mb-3">
+            These cookies help us to customize and enhance your online
+            experience with the Spectraflic service. For example, they help us
+            to remember your preferences and prevent you from needing to
+            re-enter information you previously provided (for example, during
+            member sign up). We also use these cookies to collect information
+            (such as popular pages, conversion rates, viewing patterns,
+            click-through and other information) about our visitors' use of the
+            Spectraflic service so that we can provide our service and also to
+            research, analyze and improve our services. Deletion of these types
+            of cookies may result in limited functionality of our service.
+            Lifespan: Most cookies are only active for one day. Some cookies are
+            active for a longer time, ranging from 3 to 12 months.
+          </p>
+
+          <hr className="bg-darkAlt h-[1px] my-7" />
+
+          <h4 className="heading mb-4">
+            Third Party Performance and Functionality Cookies
+          </h4>
+          <p className="mb-3">
+            These cookies, set by third parties, help us to customize and
+            enhance your online experience with Spectraflic. The cookies in this
+            category are only set on Tudum (our official fandom site). We use
+            these cookies to provide you experiences hosted by third parties,
+            like displaying social media content. For further information on how
+            these third parties use such cookies, please see the privacy
+            information provided by the third party on their website. Deletion
+            of these types of cookies may result in limited functionality.
+          </p>
+
+          <hr className="bg-darkAlt h-[1px] my-7" />
+
+          <h4 className="heading mb-4">Advertising Cookies</h4>
+          <p className="mb-3">
+            These cookies collect information via the Spectraflic service in
+            connection with "Advertisements" (as defined in our Terms of Use).
+            "Advertising Companies" (as defined in our Privacy Statement) may
+            also collect information via these cookies in connection with
+            Advertisements. If you opt out of advertising cookies, you may still
+            see Advertisements on the Spectraflic service but they will not be
+            based on information collected from advertising cookies.
+          </p>
+          <p className="mb-3">
+            "Spectraflic Marketing Providers" (also defined in the our Privacy
+            Statement) may also collect information via advertising cookies in
+            connection with Spectraflic marketing campaigns promoting the
+            Spectraflic service or Spectraflic content, such as our ads on third
+            party services. If you opt out of advertising cookies, you may still
+            see Spectraflic marketing campaigns promoting the Spectraflic
+            service or Spectraflic content, but they will not be based on
+            information collected from these advertising cookies.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default CookiePreferences;
