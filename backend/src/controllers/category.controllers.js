ce8d6abb5950262565fe57741f6d62df2d496057
Rahul Yadav <arabicrahul04@gmail.com>
2025-02-07 17:34:25 +0530

    Backend - Added Create Category API

--- backend/src/controllers/category.controllers.js
+++ backend/src/controllers/category.controllers.js
@@ -2,6 +2,7 @@ import Category from "../models/category.model.js";
 import ApiError from "../utils/apiError.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiResponse from "../utils/apiResponse.js";
+import { notEmptyValidation } from "../utils/validators.js";
 
 // Fetch All Categories Controller
 export const fetchCategoriesController = asyncHandler(async (req, res) => {
@@ -19,8 +20,8 @@ export const fetchCategoriesController = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, categories, "Fetched Categories Successfully!"));
 });
 
-// Fetch Single Category Controller
-export const fetchSingleCategoryController = asyncHandler(async (req, res) => {
+// Fetch Category Detail Controller
+export const fetchCategoryDetailController = asyncHandler(async (req, res) => {
   const { _id } = req.params;
 
   /**
@@ -40,4 +41,29 @@ export const fetchSingleCategoryController = asyncHandler(async (req, res) => {
   res
     .status(200)
     .json(new ApiResponse(200, category, "Fetched Category Successfully!"));
+});
+
+// Create Category Controller
+export const createCategoryController = asyncHandler(async (req, res) => {
+  const { name, description } = req.body;
+
+  /**
+   * TODO: Validate Input
+   * TODO: Create New Category
+   * TODO: Send Response
+   * **/
+
+  // * Validate Input
+  notEmptyValidation([name, description]);
+  const nameExist = await Category.findOne({ name });
+  if (nameExist) throw new ApiError(409, `${name} category already exists`);
+
+  // * Create New Category
+  const newCategory = new Category({ name, description });
+  await newCategory.save();
+
+  // * Sending Response
+  res
+    .status(201)
+    .json(new ApiResponse(201, newCategory, "Category created successfully!"));
 });
--- backend/src/routes/category.routes.js
+++ backend/src/routes/category.routes.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import {
+  createCategoryController,
   fetchCategoriesController,
-  fetchSingleCategoryController,
+  fetchCategoryDetailController,
 } from "../controllers/category.controllers.js";
 import { isAdmin, verifyUser } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
 
 // Routes
-router.route("/").get(fetchSingleCategoryController);
+router.route("/").get(fetchCategoryDetailController);
+router.route("/").post(verifyUser, isAdmin, createCategoryController);
 router.route("/all").get(fetchCategoriesController);
 
 export default router;
