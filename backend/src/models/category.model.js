4184cde50bd5981af3f9fc0bec283cdff60a7a55
Rahul Yadav <arabicrahul04@gmail.com>
2025-02-07 16:03:09 +0530

    Backend - Added Category Model

---
+++ backend/src/models/category.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const CategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Category = mongoose.model("Category", CategorySchema);
+
+export default Category;
