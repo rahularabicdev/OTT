b7ab8fb4c1c23568c56da12022852b0a886c0504
Rahul Yadav <arabicrahul04@gmail.com>
2025-02-08 12:10:31 +0530

    Backend - Added Update Genre API

--- backend/src/controllers/genre.controllers.js
+++ backend/src/controllers/genre.controllers.js
@@ -49,3 +49,76 @@ export const fetchGenresDetailController = asyncHandler(async (req, res) => {
     .status(200)
     .json(new ApiResponse(200, genre, "Fetched Genre Details Successfully!"));
 });
+
+// * Admin Controls
+
+// Create Genre Controller
+export const createGenreController = asyncHandler(async (req, res) => {
+  /**
+   * TODO: Validate Input
+   * TODO: Create New Genre
+   * TODO: Send Response
+   * **/
+
+  // * Get data from Frontend
+  const { name, description } = req.body;
+
+  // * Validate Input
+  notEmptyValidation([name, description]);
+  const nameExist = await Genre.findOne({ name });
+  if (nameExist) throw new ApiError(409, `${name} genre already exists`);
+
+  // * Create New Genre
+  const newGenre = new Genre({ name, description });
+  await newGenre.save();
+
+  // * Sending Response
+  res
+    .status(201)
+    .json(new ApiResponse(201, newGenre, "Genre created successfully!"));
+});
+
+// Update Genre Controller
+export const updateGenreController = asyncHandler(async (req, res) => {
+  /**
+   * TODO: Get Id from Params
+   * TODO: Get Data from Frontend
+   * TODO: Validate Input
+   * TODO: Update Genre
+   * TODO: Send Response
+   **/
+
+  // * Get Id from Params
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    throw new ApiError(400, "Invalid genre ID");
+  }
+  const genre = await Genre.findById(id);
+  if (!genre) throw new ApiError(404, "Genre not found");
+
+  // * Get data from Frontend
+  const { name, description } = req.body;
+
+  // Check if the name has changed
+  if (name !== genre.name) {
+    // * Check if the new name already exists in the database
+    const existingName = await Genre.findOne({ name });
+
+    if (existingName) {
+      return res
+        .status(400)
+        .json(new ApiResponse(400, null, "Name already exists"));
+    }
+  }
+
+  // * Update Genre
+  genre.name = name;
+  genre.description = description;
+  await genre.save();
+
+  // * Sending Response
+  res
+    .status(200)
+    .json(new ApiResponse(200, genre, "Genre updated successfully!"));
+});
--- backend/src/routes/genre.routes.js
+++ backend/src/routes/genre.routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import {
+  createGenreController,
   fetchGenreController,
   fetchGenresDetailController,
+  updateGenreController,
 } from "../controllers/genre.controllers.js";
 import { isAdmin, verifyUser } from "../middlewares/auth.middlewares.js";
 
@@ -10,5 +12,8 @@ const router = Router();
 // Routes
 router.route("/all").get(fetchGenreController);
 router.route("/:id").get(fetchGenresDetailController);
+
+router.route("/").post(verifyUser, isAdmin, createGenreController);
+router.route("/:id").patch(verifyUser, isAdmin, updateGenreController);
 
 export default router;
