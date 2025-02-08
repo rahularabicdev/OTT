8acbe1b2601ec9eeee86fc980fd32b2b94f1f8bc
Rahul Yadav <arabicrahul04@gmail.com>
2025-02-08 13:56:28 +0530

    Backend - Added Cast Model

---
+++ backend/src/models/cast.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const CastSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      default: "public/avatar/user-default.webp",
+    },
+  },
+  { timestamps: true }
+);
+
+const Cast = mongoose.model("Cast", CastSchema);
+
+export default Cast;
