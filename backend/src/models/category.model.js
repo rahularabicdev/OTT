2c84308d86764326a990100b1116b731b47598da
Rahul Yadav <arabicrahul04@gmail.com>
2025-02-11 12:37:54 +0530

    Backend - Updated Category Model

--- backend/src/models/category.model.js
+++ backend/src/models/category.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import slugify from "slugify";
 
 const CategorySchema = new mongoose.Schema(
   {
@@ -7,12 +8,36 @@ const CategorySchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
     description: {
       type: String,
     },
   },
   { timestamps: true }
 );
+
+// Unique slug generation middleware
+CategorySchema.pre("validate", function (next) {
+  if (!this.slug || this.isModified("name")) {
+    this.slug = slugify(this.name, { lower: true, strict: true });
+  }
+  next();
+});
+
+CategorySchema.pre(["findOneAndUpdate", "updateOne"], async function (next) {
+  let update = this.getUpdate();
+  if (update.name) {
+    update.slug = slugify(update.name, { lower: true, strict: true });
+  }
+  next();
+});
 
 const Category = mongoose.model("Category", CategorySchema);
 
