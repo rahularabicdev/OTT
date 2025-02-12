c352c584d1677f8dbb839fc06bf9a6d8337fdc9a
Rahul Yadav <arabicrahul04@gmail.com>
2025-02-12 12:58:21 +0530

    Frontend - Added FAQ Section

---
+++ frontend/src/app/(site)/faq/page.jsx
@@ -0,0 +1,16 @@
+import { FAQSection } from "@/sections/marketing";
+
+const FAQPage = () => {
+  return (
+    <>
+      <section className="section"></section>
+      <section className="section">
+        <div className="container">
+          <FAQSection />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default FAQPage;
---
+++ frontend/src/utility/faq-data.js
@@ -0,0 +1,46 @@
+const FaqData = [
+  {
+    _id: 1,
+    title: "What is Spectraflix?",
+    content:
+      "Spectraflix is an OTT (Over-The-Top) streaming platform that offers a wide range of movies, web series, TV shows, and documentaries. It caters to all age groups and interests, and you can watch it on your phone, tablet, laptop, or Smart TV.",
+    isOpen: true,
+  },
+  {
+    _id: 2,
+    title: "How much does Spectraflix cost?",
+    content:
+      "Spectraflix offers affordable plans starting at rs 199 per month . There are multiple options available such as Basic , Standard and Premium plans , to suit different budgets and need",
+    isOpen: false,
+  },
+  {
+    _id: 3,
+    title: "How does Spectraflix work?",
+    content:
+      "Spectraflix is a subscription-based streaming service You need to create an account and select a subscription plan. Once Subscribed you can access thousands of movies and shows anytime and anywhere , as long as you have an internet connection",
+    isOpen: false,
+  },
+  {
+    _id: 4,
+    title: "Do I need an account to watch Spectraflix?",
+    content:
+      "Yes , you need to create an account to watch content on Spectraflix . Having an account help personlize your experience , track your watch history , and provide tailored recommendations based on your preferences.",
+    isOpen: false,
+  },
+  {
+    _id: 5,
+    title: "Can I try Spectraflix for free?",
+    content:
+      "Yes, Spectraflix Offers a free trial for new users . this allows you to explore the plateform and its content before committing to a subscription .",
+    isOpen: false,
+  },
+  {
+    _id: 6,
+    title: "What kind of content is available on Spectraflix?",
+    content:
+      "Spectraflix Provides a variety of content,including movies, web series, tv shows etc. You will find content across different genres like action, drama, comedy, thriller, romance and more as well as regional and international titles.",
+    isOpen: false,
+  },
+];
+
+export default FaqData;
