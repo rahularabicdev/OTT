2696c9bc31e4f08c170df2a455b7ec198069fc4d
Rahul Yadav <arabicrahul04@gmail.com>
2025-02-28 15:54:13 +0530

    Backend - Added Upload Thumbnail API

--- backend/src/controllers/video.controllers.js
+++ backend/src/controllers/video.controllers.js
@@ -1,11 +1,10 @@
+import mongoose from "mongoose";
+
 import Video from "../models/video.model.js";
 import ApiError from "../utils/apiError.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiResponse from "../utils/apiResponse.js";
-import {
-  isFileEmptyValidation,
-  notEmptyValidation,
-} from "../utils/validators.js";
+import { notEmptyValidation } from "../utils/validators.js";
 
 // Fetch All Videos
 export const fetchAllVideosController = asyncHandler(async (req, res) => {
@@ -39,8 +38,9 @@ export const createVideoController = asyncHandler(async (req, res) => {
   notEmptyValidation([title, description, duration, category, genres]);
 
   // * Check if Video Exist
-  const existingVideo = await Video.findOne({ title });
-  if (existingVideo) throw new ApiError(409, `${title} video already exists`);
+  const existingVideo = await Video.findOne({ title, category });
+  if (existingVideo)
+    throw new ApiError(409, `${title} video already exists in this category`);
 
   // * Create New Video
   const video = new Video({
@@ -56,4 +56,39 @@ export const createVideoController = asyncHandler(async (req, res) => {
   res
     .status(201)
     .json(new ApiResponse(201, video, "Video created successfully!"));
+});
+
+// Upload Video Thumbnail Controller
+export const uploadVideoThumbnailController = asyncHandler(async (req, res) => {
+  /**
+   * TODO: Get Video Id from Query
+   * TODO: Get file from Frontend
+   * TODO: Upload File
+   * TODO: Sending Response
+   * **/
+
+  // * Get Video Id from Query
+  const { id } = req.params;
+
+  // * Validate Video Id
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    throw new ApiError(400, "Invalid video id");
+  }
+
+  // * Find Video by Id
+  const video = await Video.findById(id);
+  if (!video) throw new ApiError(404, "Video not found");
+
+  // * Get File from Frontend
+  const thumbnail = req.file?.path;
+  if (!thumbnail) throw new ApiError(400, "No thumbnail provided");
+
+  // * Update Video Thumbnail
+  video.thumbnail_url = thumbnail;
+  await video.save();
+
+  // * Sending Response
+  res
+    .status(200)
+    .json(new ApiResponse(200, video, "Video thumbnail updated successfully!"));
 });
--- backend/src/routes/video.routes.js
+++ backend/src/routes/video.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   createVideoController,
   fetchAllVideosController,
+  uploadVideoThumbnailController,
 } from "../controllers/video.controllers.js";
 import uploadMiddleware from "../middlewares/multer.middlewares.js";
 import { verifyUser, isAdmin } from "../middlewares/auth.middlewares.js";
@@ -9,11 +10,14 @@ import { verifyUser, isAdmin } from "../middlewares/auth.middlewares.js";
 const router = Router();
 
 // Upload folders
-const videoUpload = uploadMiddleware([{ name: "video", maxCount: 1 }]);
-const thumbnailUpload = uploadMiddleware([{ name: "thumbnail", maxCount: 1 }]);
+const videoUpload = uploadMiddleware("video");
+const thumbnailUpload = uploadMiddleware("thumbnail");
 
 // Routes
 router.route("/").get(fetchAllVideosController);
 router.route("/").post(verifyUser, isAdmin, createVideoController);
+router
+  .route("/:id")
+  .patch(verifyUser, isAdmin, thumbnailUpload, uploadVideoThumbnailController);
 
 export default router;
